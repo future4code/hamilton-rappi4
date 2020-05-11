14108dcc2bba7d2f3d9838ff25edd145d69df0b0
Gabriela Salvo <gabrielamdesalvo@gmail.com>
2020-05-11 14:09:11 -0300

    inicio da implementação do filtro de restaurantes

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -7,30 +7,53 @@ import { connect } from 'react-redux';
 
 
 class Home extends React.Component {
+  state = {
+
+    findRestaurant:""
+
+  }
 
   componentDidMount(){
     this.props.getRestaurants()
 
   }
 
+  handleFindRestaurant=(event)=> {
+  this.setState({findRestaurant: event.target.value})
+  }
 
     render() {
+
+      const filterRestaurants = this.props.restaurants.filter((restaurant)=>{
+       
+          const search = this.state.findRestaurant.toLowerCase()
+          return restaurant.name.toLowerCase().includes(search)
+        
+      })
+      console.log(this.state.findRestaurant)
+   
         return (
             <>
                 <div>HOME</div>
 
                 <div>
-
-                   <label>Buscar restaurantes</label>
+                   <label 
+                   htmlFor="find-restaurant"
+                   >Buscar restaurantes
+                   </label>
 
                     <input
+                    id="find-restaurant"
+                    onChange={this.handleFindRestaurant}
                     type="text"
+                    value={this.state.findRestaurant}
+                    
                     />
                     <div>
 
-                     {this.props.restaurants.map((restaurant)=> {
+                     {filterRestaurants.map((restaurant)=> {
                        return (
-                         <Cards restaurant={restaurant}/>
+                         <Cards key={restaurant.id} restaurant={restaurant}/>
                        )
                      })}
 
